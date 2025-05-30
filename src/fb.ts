72b892393380615d311dd48f5a2f87398247ddac
Senthilkumar M <senthilkumarmac@Senthilkumars-Mac-mini.local>
2025-05-30 18:47:58 +0530

    hosting

---
+++ src/fb.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZ3Ysnh-p-rqM7ctzSOiyOq5Ci7hDMROI",
+  authDomain: "landingverse.firebaseapp.com",
+  projectId: "landingverse",
+  storageBucket: "landingverse.firebasestorage.app",
+  messagingSenderId: "991001953176",
+  appId: "1:991001953176:web:455d2b4b4a3f4ce67b21ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
